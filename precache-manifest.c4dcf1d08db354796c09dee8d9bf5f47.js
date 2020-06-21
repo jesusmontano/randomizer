1ea1405c41565446932020bcfb7537f93917724d
Jesus Montano <jesus.montano@tradeshift.com>
2020-06-21 12:58:35 -0700

    Updates

---
+++ precache-manifest.c4dcf1d08db354796c09dee8d9bf5f47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfe6b73fddc09ad5a0dbc0206acd40ee",
+    "url": "/randomizer/index.html"
+  },
+  {
+    "revision": "c873602140d11917b6f6",
+    "url": "/randomizer/static/css/main.ac7d2f2e.chunk.css"
+  },
+  {
+    "revision": "064f0f5f0a171c9d74fa",
+    "url": "/randomizer/static/js/2.086e4ccf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/randomizer/static/js/2.086e4ccf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c873602140d11917b6f6",
+    "url": "/randomizer/static/js/main.65a723b8.chunk.js"
+  },
+  {
+    "revision": "973837aac4b5d7c7a285",
+    "url": "/randomizer/static/js/runtime-main.c4837e7c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/randomizer/static/media/logo.5d5d9eef.svg"
+  }
+]);
